db30cb808c12f322e170c59ffc01cc2f6f23a345
DeamonX <cofari12@gmail.com>
2023-01-16 21:30:19 +0100

    commit

--- frontend/src/models/routes.tsx
+++ frontend/src/models/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Fooldal from '../routes/Home';
 import { categoryLoader, authorLoader, bookLoader } from './routeLoaders';
-import React from 'react';
 import Book from '../routes/Book';
 import Author from '../routes/Author';
 import Category from '../routes/Category';
